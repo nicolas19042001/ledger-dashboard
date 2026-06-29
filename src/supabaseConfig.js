7f49b08571069fe6dbb567a74c2d9d4bc0582b21
nicolas19042001 <sr5srgrg5z@privaterelay.appleid.com>
2026-06-29 13:57:32 +0200

    Create supabaseConfig.js

---
+++ src/supabaseConfig.js
@@ -0,0 +1,13 @@
+// ====================================================================
+//  HIER DEINE BEIDEN WERTE AUS SUPABASE EINTRAGEN
+//  (Project Settings -> API).
+//  Beide Werte sind für die Nutzung im Browser gedacht und kein Geheimnis.
+// ====================================================================
+
+// Deine Project URL, z.B. "https://maywlzcilchopdxphobl.supabase.co"
+export const SUPABASE_URL = "https://maywlzcilchopdxphobl.supabase.co";
+
+// Dein "anon public" Schlüssel (langer Text, beginnt mit eyJ...)
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1heXdsemNpbGNob3BkeHBob2JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MjkzOTEsImV4cCI6MjA5ODMwNTM5MX0.HgKsSvVf2VlmuJZYhPCELXoD_x-IbO2JNhJyw7bpb_Y";
+
+
